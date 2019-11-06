1fc7dc19e9dd35cd0ac405de9647c4bb1f97b58a
michele <michele.provolo@gmail.com>
2019-11-06 09:29:10 +0100

    revisione1

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-//voglio che sia mostrato .header-right > a che rappresenta l'hamburger menu
 
-$( ".header-right > a" ).show();
-
-
+//voglio che al clic si apra l'hamburger menu
 $( ".header-right > a" ).click(function() {
     $( ".hamburger-menu" ).show();
 });
 
-
+//voglio che al clic si chiuda l'hamburger menu
 $( ".close" ).click(function() {
     $( ".hamburger-menu" ).hide();
 });
